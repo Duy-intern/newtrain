eaf488cf804a90e4c59c601898fa84934b1b3d76
Duy-intern <quocduy080819@gmail.com>
2024-10-19 23:09:31 +0700

    fix

--- dist/frontend/src/app/components/hoteldetailbutton.d.ts
+++
@@ -1,1 +0,0 @@
-export default function DetailButton(): import("react/jsx-runtime").JSX.Element;
--- dist/frontend/src/app/components/imagehomepage.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const HomeCard: React.FC;
-export default HomeCard;
